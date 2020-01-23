7a0d6d7de0e4113d0ed2f6104f415b682258213b
AleksandarMiha <miha1993xd@hotmail.com>
2020-01-23 23:12:29 +0100

    updates for production3

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ipro'));
+app.use(express.static(__dirname + '/dist/ipros'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/ipro/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ipros/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
